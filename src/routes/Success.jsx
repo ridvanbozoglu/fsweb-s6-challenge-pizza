add4301934ae7b9303d71e3bfa985d18e22dac06
ridvanbozoglu <ridvanbozoglu.5@gmail.com>
2024-07-14 09:27:16 +0300

    if there is no order directed to order page at success page

--- src/routes/Success.jsx
+++ src/routes/Success.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Footer } from "../components";
 import TopBar from "../components/ui/TopBar";
 import SuccessContent from "../components/succesPage/SuccessContent";
+import { useNavigate } from "react-router-dom";
 
 const Success = ({ state }) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!state) {
+      navigate("/order");
+    }
+  }, [state]);
+
   return (
     <div className="succes-route-container">
       <TopBar />
-      <SuccessContent data={state} />
+      {state && <SuccessContent data={state} />}
       <Footer />
     </div>
   );
